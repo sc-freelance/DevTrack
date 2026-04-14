244748a998e1bd988eba224d3e99df71080e9749
sc-freelance <saadchughtai756@gmail.com>
2026-04-14 12:31:13 +0100

    feat(app): Connect pages to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Import your page components
 import Home from './pages/home';
-import Navbar from './components/navbar';
+import Dashboard from './components/dashboard/dashboard';
+import Login from './pages/login';
+import Register from './pages/register';
+import Categories from './pages/categories';
+import Settings from './pages/settings';
 // Import Sidebar and SidebarProvider from your UI folder
 import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import Navbar from './components/navbar';
 import './index.css';
 import './App.css';
 
@@ -16,6 +22,14 @@ function App() {
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<div>Projects Page</div>} />
+              <Route path="/tasks" element={<div> Tasks Page </div>} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/login" element={<Login />} />
+              <Route path='register' element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/contacts" element={<div> Contacts Page </div>} />
             </Routes>
           </main>
         </div>

9557499cf9e05cf36f40ee198c04485bc88fffda
sc-freelance <saadchughtai756@gmail.com>
2026-06-03 11:59:33 +0100

    feat(tests): Create the test class

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import *, Project
 
 # Create your tests here.
+class ProjectModelTest(TestCase):
+    def test_project_creation(self):
+        project = Project.objects.create(
+            title = "DevTrack",
+            description = "A project management tool for developers.",
+            status = "active"
+        )
+        self.assertEqual(project.title, "DevTrack")
+        self.assertEqual(project.description, "A project management tool for developers.")
+        self.assertEqual(project.status, "active")
